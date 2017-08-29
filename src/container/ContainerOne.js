e4e55696767e7fce13f352df1ac686123cd665cc
yellowfrog <174700701@qq.com>
2017-08-29 10:18:02 +0800

    feat: 加入react-dnd

--- src/container/ContainerOne.js
+++ src/container/ContainerOne.js
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import CSSModules from 'react-css-modules';
+import styles from './ContainerOne.less';
 
-const ContainerOne = () => <div>ContainerOne</div>
+class ContainerOne extends Component {
 
-export default ContainerOne
+    state = {
+        count: 1
+    }
+
+    render() {
+        return (
+            <div styleName='container-one'>
+                <div className='item'>1</div>
+                <div className='item'>2</div>
+                <div className='item'>3</div>
+                <div className='item'>4</div>
+                <div className='item'>5</div>
+                <div className='item'>6</div>
+                <div className='item'>7</div>
+                <div className='item'>8</div>
+                <div className='item'>9</div>
+                <div className='item'>10</div>
+                <div className='item'>11</div>
+                <div className='item'>12</div>
+            </div>
+        );
+    }
+}
+
+export default CSSModules(ContainerOne, styles);
